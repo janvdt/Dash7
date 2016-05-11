cd3432838f5f887216c0e805cc5cf60dfff19213
kevinheyvaert <kevin-heyvaert@hotmail.com>
2016-05-11 13:08:03 +0200

    Changed message structure

--- src/apps/project/project.c
+++ src/apps/project/project.c
@@ -67,16 +67,19 @@ double sensor_values[96];	//ben niet zeker van datatype..of als string doorsture
 							
 /*
 Message structure 96 bits
-Place of bits 	(x:x) & possible value (xx)
--Control bits 	(1:8) : Water in tophalf (0/1), Battery Charching (0/1) , Percentage of battery in 5% steps (from 0 to 20)(00000/10100) , one bit left
-- Water temp 	(9:16) : from -50 to 125 �C --> 8 bits (1100 1100 to 0111 1101 )
-- CL			(17:25) : Reserve for CL (can be changed) --> can be used for temperature (1 getal na de komma erbij --> 4 bits) + 4 bits vrij
-- pH 			(25:32) : pH value from 3,9 to 10,9 pH (8 bits)
-- Accelerometer	(33:40) :  calculate the G force as an directionless measurement --> 1 byte of data:double g = Math.sqrt(x * x + y * y + z * z);
-- Flow meter	(41:50) : from 0,00 to 6,00L (10 0101 1000)
-- Latitude(GPS) (51:70) : -90,00000 to + 90,00000� --> 20 bits (0001 0101 1111 1001 0000 to 1001 0101 1111 1001 0000)
-- Longitude(GPS)(71:94) :  -180,0000 to + 180,0000� --> 24 bits (0001 1011 0111 0111 0100 0000? tot 1001 1011 0111 0111 0100 0000)
- - open			(95:96) : can be used
+Place of bits 	(x:x)  - data[x] & possible value (xx)
+-Control bits 	(1:8)  - data[0]: Water in tophalf (0/1), Battery Charching (0/1) , Percentage of battery in 5% steps (from 0 to 20)(00000/10100) , one bit left
+- Water temp 	(9:16) - data[1] : from -50 to 125 �C --> 8 bits (1100 1100 to 0111 1101 )
+- pH 			(17:25)- data[2] : pH value from 3,9 to 10,9 pH (8 bits)
+- Latitude(GPS) (26:49)- data[3] : -90,00000 to + 90,00000� --> 20 bits (0001 0101 1111 1001 0000 to 1001 0101 1111 1001 0000)
+					   - data[4]
+					   - data[5]
+- Longitude(GPS)(50:72)- data[6] : -180,0000 to + 180,0000� --> 24 bits (0001 1011 0111 0111 0100 0000? tot 1001 1011 0111 0111 0100 0000)
+						 data[7]
+- Flow meter	(73:80)- data[8] : from 0,00 to 6,00L (10 0101 1000)
+- Accelerometer	(81:96)- data[9] :  calculate the G force as an directionless measurement --> 1 byte of data:double g = Math.sqrt(x * x + y * y + z * z);
+					   - data[10]
+
 */
 
 // Toggle different operational modes
